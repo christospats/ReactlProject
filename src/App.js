3240958eb054c5cf9533cefed8afaf7d74b509b5
Thomakos <tomovige@gmail.com>
2022-09-12 18:30:51 +0300

    Fixes #2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from './comps/Navigation';
 import {Route, Routes} from 'react-router-dom';
 import "rsuite/dist/rsuite.min.css";
 import 'rsuite/styles/index.less';
+import Communication from './comps/Communication';
 
 function App() {
   return(
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<>
         <Navigation />
         <HomePage />
+        <Communication />
       </>}/>
     </Routes>
   )
---
+++ src/comps/Communication.jsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react';
+
+function Communication() {
+    return ( 
+        <h1>Communication</h1>
+     );
+}
+
+export default Communication;
