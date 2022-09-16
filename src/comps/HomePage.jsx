7ea9d68a7c9e688305494f940a44ab123161a19e
christos fanourios patsouras <xristos.patsoyras@gmail.com>
2022-09-16 16:23:55 +0300

    Fixes #12

--- src/comps/HomePage.jsx
+++ src/comps/HomePage.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Container, Content } from 'rsuite';
 
 function HomePage() {
     return ( 
-        <div>
-            <h1>Πληροφορίες</h1>
-        </div>
+        <Container>
+            <Content 
+                style={{backgroundImage: 'url("https://ered.gr/media/AAA%20JPG/school.jpg")',
+                        backgroundPosition:'center',
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat',
+                        width: '100%',
+                        height: '100vh',
+                    }}
+            >
+                <Content 
+                    className='content'
+                    style={{backgroundColor:"white",width:'50%',height:"70%",borderRadius: "25px", marginTop:'5em', marginLeft:"25%"}}
+                >
+                    <h3 style={{textAlign:'center'}}>Πληροφορίες φροντιστήριου</h3>
+                    <p style={{textAlign:'center'}}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br/> 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/> 
+                        Ut enim ad minim veniam,<br/>
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br/> 
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br/> 
+                        Excepteur sint occaecat cupidatat non proident,<br/> 
+                        sunt in culpa qui officia deserunt mollit anim id est laborum.<br/>
+                    </p>
+                </Content>
+            </Content>
+        </Container>
      );
 }
 
