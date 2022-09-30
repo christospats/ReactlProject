0546af3620bd4b091cf10f8dcd1ace35f6d31062
Christos Fanourios Patsouras <donchokomaster69@gmail.com>
2022-09-30 10:49:59 +0300

    Added Carousel at Home Page

--- src/comps/HomePage.jsx
+++ src/comps/HomePage.jsx
@@ -4,8 +4,8 @@ import { Container, Content, FlexboxGrid,Carousel } from 'rsuite';
 function HomePage() {
     return ( 
         <FlexboxGrid>
-            <Container style={{marginTop:'56px',marginBottom:'56px',width:"100%", height:"100%",position:'static'}}>
-                <Carousel autoplay className="custom-slider" style={{position:'absolute'}}>
+            <Container style={{marginTop:'56px',position:'static',width:'100%',height:'100vh'}}>
+                <Carousel autoplay shape="bar" className="custom-slider" style={{position:'absolute',width:"100%", height:"100%"}}>
                     <img src='https://scontent.fskg1-1.fna.fbcdn.net/v/t31.18172-8/10258762_1391223734496916_2223556813288643439_o.jpg?_nc_cat=102&ccb=1-7&_nc_sid=19026a&_nc_ohc=eUL7ZpDLINYAX-PWFq9&tn=lGs49qPfaD2b0l28&_nc_ht=scontent.fskg1-1.fna&oh=00_AT_3vhCNrMU4OEdgCaQf2YEpHbmqHPi4CgwKz2o15SMMKA&oe=63582A98'alt="Classrooms" />
                     <img src='https://scontent.fskg1-2.fna.fbcdn.net/v/t31.18172-8/1973621_1391159501170006_7742655064997543171_o.jpg?_nc_cat=103&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=B85YqIFLKiMAX8QcdsL&_nc_ht=scontent.fskg1-2.fna&oh=00_AT-iV539uvEcHG3yvDpawLxDmrw-dP5nw5RvJuEY9a9-wA&oe=6357B080' alt="Classrooms" />
                     <img src='https://scontent.fskg1-2.fna.fbcdn.net/v/t31.18172-8/10275455_1391154691170487_6968689000093356810_o.jpg?_nc_cat=101&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=96SvL5MNHXUAX_Y5eMO&_nc_ht=scontent.fskg1-2.fna&oh=00_AT-z8b8n7JJJTzZxD02ioIUiEEu9-pxI01-Jq8MAT1TgJg&oe=6358E155' alt="Classrooms" />
@@ -14,10 +14,17 @@ function HomePage() {
                 </Carousel>
                 <Content 
                     className='content'
-                    style={{backgroundColor:"white",width:'50%',height:"70%",borderRadius: "25px", marginTop:'5em', marginLeft:"25%",zIndex:1}}
+                    style={{backgroundColor:"white", borderRadius:'25px',zIndex:1,display:"flex",opacity:'90%', flexFlow:'column',alignSelf:'center',marginTop:'145px',position:'absolute'}}
                 >
                     <h3 style={{textAlign:'center'}}>Πληροφορίες φροντιστήριου</h3>
                     <p style={{textAlign:'center'}}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br/> 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/> 
+                        Ut enim ad minim veniam,<br/>
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br/> 
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br/> 
+                        Excepteur sint occaecat cupidatat non proident,<br/> 
+                        sunt in culpa qui officia deserunt mollit anim id est laborum.<br/>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br/> 
                         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/> 
                         Ut enim ad minim veniam,<br/>
