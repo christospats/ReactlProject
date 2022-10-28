9b97afe756971fd08de0e78aeea5dee188b0369a
Christos Fanourios Patsouras <donchokomaster69@gmail.com>
2022-10-28 10:31:53 +0300

    Fixes #30

--- src/comps/HomePage.jsx
+++ src/comps/HomePage.jsx
@@ -18,22 +18,27 @@ function HomePage({homeRef}) {
                         className='content'
                         style={{backgroundColor:"white", borderRadius:'25px',zIndex:1,display:"flex",opacity:'90%', flexFlow:'column',alignSelf:'center',marginTop:'145px',position:'absolute'}}
                     >
-                        <h3 style={{textAlign:'center'}}>Πληροφορίες φροντιστήριου</h3>
-                        <p style={{textAlign:'center'}}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br/> 
-                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/> 
-                            Ut enim ad minim veniam,<br/>
-                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br/> 
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br/> 
-                            Excepteur sint occaecat cupidatat non proident,<br/> 
-                            sunt in culpa qui officia deserunt mollit anim id est laborum.<br/>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br/> 
-                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/> 
-                            Ut enim ad minim veniam,<br/>
-                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br/> 
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br/> 
-                            Excepteur sint occaecat cupidatat non proident,<br/> 
-                            sunt in culpa qui officia deserunt mollit anim id est laborum.<br/>
+                        <h3 style={{textAlign:'center',fontFamily:'Halvetica'}}>Το φροντιστήριό μας</h3>
+                        <p style={{textAlign:'center',fontSize:'large',fontFamily:'Halvetica'}}>
+                        <br/>Από το 2015 στο κέντρο του Βόλου λειτουργούμε το φιλολογικό μας φροντιστήριο μέσης<br/>
+                        εκπαίδευσης. Για πάνω από 15 χρόνια εργαστήκαμε στο χώρο της ιδιωτικής εκπαίδευσης, έως την<br/>
+                        ώρα που αποφασίσαμε να «αυτονομηθούμε». Στο φροντιστήριό μας διδάσκονται τα μαθήματα των<br/>
+                        Αρχαίων Ελληνικών, της Ιστορίας, των Λατινικών, της Νεοελληνικής Γλώσσας και Λογοτεχνίας, στα<br/>
+                        οποία και εξετάζονται στις πανελλαδικές οι υποψήφιοι του 1ου πεδίου (ανθρωπιστικών σπουδών).<br/>
+                        Προσπαθήσαμε και προσπαθούμε να προσφέρουμε στους μαθητές μας ένα ζεστό, «σπιτίσιο» χώρο,<br/>
+                        όπου θα μπορούν να αισθάνονται άνετα και οικεία, παραμένοντας σε αυτόν πριν και μετά το πέρας<br/>
+                        των μαθημάτων τους, ώστε να διαβάζουν ή να καλύπτουν τα κενά τους. Άλλωστε, και για εμάς ο<br/>
+                        χώρος αυτός αποτελεί «δεύτερο» ή και κανονικό σπίτι !<br/>
+                        Θέλουμε να παρέχουμε στους μαθητές μας το καλύτερο δυνατό, επιχειρώντας να τους «πείσουμε»<br/>
+                        για την αξία της μόρφωσης και της κατάρτισης, που απαραίτητα όμως χρειάζεται να συμπορεύεται<br/>
+                        με το ήθος και την ανθρωπιά. Παρέχουμε σε μαθητές και γονείς έναν ηλεκτρονικό Οδηγό<br/>
+                        Σταδιοδρομίας πληρέστατο, ο οποίος διαρκώς επικαιροποιείται, γιατί πιστεύουμε πολύ στη σωστή<br/>
+                        στοχοθεσία. Στην εποχή της πληροφορίας επιβάλλεται κανείς να γνωρίζει, ώστε να κυνηγήσει τους<br/>
+                        στόχους του με ρεαλισμό και όραμα μαζί ! Οργανώνουμε το μάθημα σε ολιγομελή και ομοιογενή<br/>
+                        τμήματα, επιδιώκοντας το «δέσιμο» των μαθητών, τη συνεργασία και την αλληλοϋποστήριξη,<br/>
+                        δεδομένου ότι η πορεία προς τις εξετάσεις αποβαίνει συχνά ψυχοφθόρα.<br/>
+                        Δεν τρέφουμε την ψευδαίσθηση ότι θα πετύχουμε με όλους όσοι ζητούν τη βοήθειά μας, αλλά δε<br/>
+                        σταματούμε να προσπαθούμε – και αυτό ζητούμε από τους μαθητές μας ...<br/>
                         </p>
                     </Content>
                 </Container>
