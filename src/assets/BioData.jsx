f29dd187b781635ab206b9eaaf8e810495fdc173
Christos Fanourios Patsouras <donchokomaster69@gmail.com>
2022-10-30 10:42:20 +0200

    Added information and build the concept of BioPage

--- src/assets/BioData.jsx
+++ src/assets/BioData.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import { Content, FlexboxGrid, Panel } from 'rsuite';
 import ArrowRightLineIcon from '@rsuite/icons/ArrowRightLine';
 import { Icon } from '@rsuite/icons';
+import { useNavigate } from "react-router-dom";
 
 function BioData({profData}) {
+    let navigate = useNavigate();
+
     const handleClick = () =>{
         if(profData.name === "Γιώργος Λιάππης"){
-            window.location.replace("/")
+            navigate("/biographypageliappis");
         }
         if(profData.name === "Αμαλία Τσικρικα"){
-            window.location.replace("https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Controlling_Ratios_of_Flex_Items_Along_the_Main_Ax")
+            navigate("/biographypageamalia")
         }
     }
+
     return (
         <FlexboxGrid.Item>
             <Panel shaded bordered bodyFill style={{ display: 'inline-block', backgroundColor:"white",borderRadius:"10px" }}>
-                <img src={profData.picUrl} width="240" height="240" />
+                <img src={profData.picUrl} width="250" height="250" />
                 <Panel header={profData.name} style={{textAlign:'center'}}>
                     <p>
                         <small>
