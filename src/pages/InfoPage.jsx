4e2a23fb34c3d916dc1a8a07c1240555918318b0
Christos Fanourios Patsouras <donchokomaster69@gmail.com>
2022-11-26 12:22:47 +0200

    Fixes #53

--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -1,15 +1,36 @@
 import {Content} from 'rsuite'
+import React,{useEffect, useState} from 'react';
 
 function InfoPage({schData}) {
+    const [matches, setMatches] = useState(window.matchMedia("(min-width:768px)").matches)
+
+    useEffect(() => {
+        window.matchMedia("(min-width: 768px)").addEventListener('change', e => setMatches(e.matches));
+    }) 
+
     return ( 
-        <div style={{marginTop:'4em',
-                    border:'solid',
-                    borderRadius:"10px",
-                    padding:'2em',
-                    boxShadow: "10px 5px 30px black",
-                    backgroundColor:"#e1eedd"}}>
-            <h3 style={{textAlign:'center'}}>{schData.title}</h3>
-            <Content>{schData.content}</Content>
+        <div>
+            {matches && 
+            (<div class='heading' style={{
+                        marginLeft:'15%',
+                        marginRight:'15%',
+                        border:'solid',
+                        borderRadius:"10px",
+                        padding:'2em',
+                        boxShadow: "10px 5px 30px black",
+                        backgroundColor:"#e1eedd"}}>
+                <h3 style={{textAlign:'center'}}>{schData.title}</h3>
+                <Content>{schData.content}</Content>
+            </div>)}
+            {!matches &&
+             (<div class='heading' style={{
+                        border:'solid',
+                        padding:'2em',
+                        boxShadow: "10px 5px 30px black",
+                        backgroundColor:"#e1eedd"}}>
+                <h3 style={{textAlign:'center'}}>{schData.title}</h3>
+                <Content>{schData.content}</Content>
+            </div>)}
         </div>
     );
 }
