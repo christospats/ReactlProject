a9e4615238740034a8fd3871082bfd5c21c6d163
Christos Fanourios Patsouras <donchokomaster69@gmail.com>
2022-11-26 12:10:37 +0200

    Fixes #48

--- src/comps/Navigation.jsx
+++ src/comps/Navigation.jsx
@@ -1,8 +1,6 @@
 import React,{useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, FlexboxGrid,Dropdown } from 'rsuite';
-import ResponsiveNav from '@rsuite/responsive-nav';
-import { WheelHandler } from 'rsuite/esm/DOMHelper';
 
 
 
