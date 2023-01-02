5582f933b2293c6569035e579a652adf9f846c71
christos fanourios patsouras <xristos.patsoyras@gmail.com>
2023-01-02 19:58:24 +0200

    Fixes #100

--- src/assets/BioInfoPage.jsx
+++ src/assets/BioInfoPage.jsx
@@ -15,7 +15,7 @@ function BioInfoPage({correctProf}) {
                                 <Sidebar style={{ display:'flex',alignItems:'center',flexDirection:'column', backgroundColor:"#166534",marginTop:'56px',color:'black'}}>
                                     <img src={correctProf.picUrl} alt={correctProf.name} style={{borderRadius:'100px', height:'10em', width:'10em', marginTop:'15px'}}/>
                                     <Content>
-                                        Τηλέφωνο: 6944719951<br/>Κοινωνικά δίκτυα: <a href="https://www.facebook.com/profile.php?id=100008279084746" style={{color:'#312e81'}}>facebook</a>
+                                        Τηλέφωνο: <a href="tel:PHONE_NUM" style={{color:"#FFFFFF"}}> 6944719951 </a><br/>Κοινωνικά δίκτυα: <a href="https://www.facebook.com/profile.php?id=100008279084746" rel="noreferrer" target="_blank" style={{color:'#FFFFFF'}}>facebook</a>
                                     </Content>                                  
                                 </Sidebar>
                                     <Container style={{marginTop:'56px',background: "linear-gradient(to right, #166534 20%,#22c55e)", textAlign:'justify'}}>
@@ -32,7 +32,7 @@ function BioInfoPage({correctProf}) {
                                 <Content style={{ display:'flex',alignItems:'center',justifyContent:'center',flexDirection:'row', gap:'10px',backgroundColor:"#166534",marginTop:'56px',color:'black'}}>
                                     <img src={correctProf.picUrl} alt={correctProf.name} style={{borderRadius:'100px', height:'8em', width:'8em', marginTop:'15px'}}/>
                                     <Content>
-                                        Τηλέφωνο: 6944719951<br/>Κοινωνικά δίκτυα: <a href="https://www.facebook.com/profile.php?id=100008279084746" style={{color:'#312e81'}}>facebook</a>
+                                       Τηλέφωνο: <a href="tel:PHONE_NUM" style={{color:"#FFFFFF"}}> 6944719951 </a> <br/>Κοινωνικά δίκτυα: <a href="https://www.facebook.com/profile.php?id=100008279084746" target="_blank" rel="noreferrer" style={{color:'#FFFFFF'}}>facebook</a>
                                     </Content>
                                 </Content>
                                 <Container style={{background: "linear-gradient(to bottom, #166534 20%,#22c55e)", textAlign:'justify'}}>
